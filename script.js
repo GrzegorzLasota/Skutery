89f62b7cdbeaf2983d1635c97861452aa252363d
Grzegorz Lasota <grzegorz.lasota@o2.pl>
2024-12-12 16:17:36 +0100

    timer with days and hours start to make subpages - keeway

--- script.js
+++ script.js
@@ -59,15 +59,50 @@ class Clock {
   }
 }
 
-// function playAudio(src) {
-//   let audio = new Audio(src);
-//   audio.load();
-//   audio.play();
-// }
-
 function setup() {
   let clock1 = new Clock();
-  clock1.start("00:01");
+  clock1.start("00:59");
 }
 
-window.addEventListener("DOMContentLoaded", setup);
+// Jeśli chcesz włączyć odliczanie do zamknięcia salonu gdy będzie otwarty:
+
+// window.addEventListener("DOMContentLoaded", setup);
+
+////////////////////////////////////////Zegar///////////////////////////////////////////////////////
+function timer() {
+  const time = new Date();
+
+  const year = time.getFullYear();
+
+  elRok = document.getElementById("year");
+  elRok.textContent = year + " ";
+}
+
+document.body.onload = timer;
+
+//////////////////////////////////////////odlicznaie dni///////////////////////////////////////////////////////
+
+function days() {
+  // Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec
+  const current = new Date();
+  const aim = new Date("Mar 1 2025 09:00:00");
+
+  // wyliczanie roznicy
+  let secondsLeft = Math.abs(current - aim) / 1000;
+  let minutesLeft = parseInt(secondsLeft / 60);
+  let hoursLeft = parseInt(minutesLeft / 60);
+  let daysLeft = parseInt(hoursLeft / 24);
+  let yearsLeft = parseInt(daysLeft / 365);
+
+  // wyliczanie calego okresu
+  secondsLeft = parseInt(secondsLeft - minutesLeft * 60);
+  minutesLeft = parseInt(minutesLeft - hoursLeft * 60);
+  hoursLeft = parseInt(hoursLeft - daysLeft * 24);
+  daysLeft = parseInt(daysLeft - yearsLeft * 365);
+
+  const timeDays = document.querySelector(".hours");
+
+  timeDays.textContent = `${daysLeft}d i ${hoursLeft}g`;
+}
+
+window.addEventListener("DOMContentLoaded", days);
